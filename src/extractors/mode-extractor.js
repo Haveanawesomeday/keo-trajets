574996f5578a95ae088bc3ba3c92d449a089cfa2
Romain Nervil <romain@botfuel.io>
2018-07-04 16:49:30 +0200

    started corpus-extractor.js

--- src/extractors/mode-extractor.js
+++ src/extractors/mode-extractor.js
@@ -1,9 +1,16 @@
-const { WsExtractor } = require('botfuel-dialog');
+const { CorpusExtractor, FileCorpus } = require('botfuel-dialog');
 
-class ModeExtractor extends WsExtractor {}
-
-ModeExtractor.params = {
-  dimensions: ['travel-mode'],
-};
-
-module.exports = ModeExtractor;
+class DimensionNameExtractor extends CorpusExtractor {
+  constructor() {
+    super({
+      dimension: 'dimension_name',
+      corpus: new FileCorpus(`${__dirname}/../corpora/dimension_name.txt`),
+      options: {
+        caseSensitive: false,
+        keepQuotes: false,
+        keepDashes: false,
+        keepAccents: false,
+      },
+    });
+  }
+}
